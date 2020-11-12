cf572e54fe6dba7d8407c5843bf741a2ee23f3c0
Christine <ctrigger85@gmail.com>
2020-11-12 21:16:56 +1000

    made my rocket

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+var doCoolStuff = function () {
+	document.getElementById('cool').className = 'cool red';
+}			
+
+var car = {
+	make: 'VW',
+	type: 'Polo',
+	color: 'blue',
+	isTurnedOn: false,
+				numberOfWheels: 4, 
+				Seats: [
+				'seat 1',
+				'seat 2',
+				'seat 3',
+				'seat 4'],
+
+				turnOn: function () {
+					this.isTurnedOn = true;
+				},
+
+				fly: function () {
+					alert ('fly');
+				},
+
+				switchCar: function (isOn) {
+					if (isOn == true) {
+						this.isTurnedOn = true;
+					} else {
+							this.isTurnedOn = false;
+						}
+					}
+				}
